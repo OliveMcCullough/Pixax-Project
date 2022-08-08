fb18a3f6b365cf186ac0733288afccc8f5eee86f
Olive McCullough <olive.m.mccullough@gmail.com>
2022-08-08 19:03:10 +0100

    Implemented toggleable menu for phones Corrected issue where columns weren't taking maximum available space

--- pixax/static/js/pixax.js
+++ pixax/static/js/pixax.js
@@ -1,9 +1,41 @@
+breakpoint_phone = 768
+breakpoint_desktop = 1020
+
 window.addEventListener("load", init)
 
 function init() {
     setup_help_icons()
     setup_errored_inputs()
+    setup_phone_toggle_menu()
 }
+
+/* Toggling the phone menu */
+
+function setup_phone_toggle_menu() {
+    window.addEventListener('resize', closePhoneToggleMenuIfNotPhone)
+    let menu_icon = document.querySelector(".menu_icon");
+    menu_icon.addEventListener('click', togglePhoneMenu);
+}
+
+function togglePhoneMenu() {
+    let item_list = document.querySelector(".item_list");
+    if (item_list.classList.contains("item_list_open")) {
+        item_list.classList.remove("item_list_open")
+    }
+    else {
+        item_list.classList.add("item_list_open")
+    }
+}
+
+function closePhoneToggleMenuIfNotPhone() {
+    open_item_list = document.querySelector(".item_list_open");
+    
+    if (!!open_item_list && window.innerWidth > breakpoint_phone){
+        open_item_list.classList.remove("item_list_open");
+    }
+}
+
+/* Updating forms that are corrected */
 
 function setup_errored_inputs() {
     let errored_inputs = document.querySelectorAll(".field_has_error input, .field_has_error textarea")
@@ -18,6 +50,8 @@ function remove_field_error(e) {
     form_input_container = errored_input.closest(".form_element_container")
     form_input_container.classList.remove("field_has_error")
 }
+
+/* Allow help icons to show information on hover */
 
 function setup_help_icons() {
     let help_icons_elements = document.getElementsByClassName("help_text_icon")
