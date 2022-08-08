a934493a2a62afc302e4b333e6a8f71eb95f0962
Olive McCullough <olive.m.mccullough@gmail.com>
2022-08-08 03:14:23 +0100

    Corrected url referencing mistake

--- pixax/users/urls.py
+++ pixax/users/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView, LoginView
 
 app_name = "users"
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
-    path("login/", RegisterView.as_view(), name="login"),
+    path("login/", LoginView.as_view(), name="login"),
 ]
