969a09cafa8dff08d4c5481887be5c0d218f9dbc
Olive McCullough <olive.m.mccullough@gmail.com>
2022-08-08 16:29:33 +0100

    SCSS changes to avoid deprecations Implemented better form error handling

--- pixax/static/js/pixax.js
+++ pixax/static/js/pixax.js
@@ -1,6 +1,25 @@
 window.addEventListener("load", init)
 
 function init() {
+    setup_help_icons()
+    setup_errored_inputs()
+}
+
+function setup_errored_inputs() {
+    let errored_inputs = document.querySelectorAll(".field_has_error input, .field_has_error textarea")
+
+    for(let i =0; i < errored_inputs.length; i++) {
+        errored_inputs[i].addEventListener("change", remove_field_error)
+    }
+}
+
+function remove_field_error(e) {
+    errored_input = e.target;
+    form_input_container = errored_input.closest(".form_element_container")
+    form_input_container.classList.remove("field_has_error")
+}
+
+function setup_help_icons() {
     let help_icons_elements = document.getElementsByClassName("help_text_icon")
     
     for(let i = 0; i < help_icons_elements.length; i++) {
