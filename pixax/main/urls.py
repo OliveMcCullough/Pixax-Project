cce666267cf4e30b52d1ea73aaaffbdeab075fca
Olive McCullough <olive.m.mccullough@gmail.com>
2022-08-18 15:40:35 +0100

    Url calls for new views

--- pixax/main/urls.py
+++ pixax/main/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import RootRedirectView, MyAlbumsView
+from .views import AlbumDetailView, RootRedirectView, MyAlbumsView, UploadPicturesView
 
 app_name = "main"
 
 urlpatterns = [
     path('', RootRedirectView.as_view(), name='root'),
     path('albums/', MyAlbumsView.as_view(), name='albums'),
+    path('albums/<int:pk>/', AlbumDetailView.as_view(), name='album'),
+    path('upload/', UploadPicturesView.as_view(), name="upload"),
 ]
