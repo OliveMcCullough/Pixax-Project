29444a1d383eadf879e7a14a90e5c1a1237329ff
Olive McCullough <olive.m.mccullough@gmail.com>
2022-08-24 03:14:19 +0100

    URL path to allow access to new views

--- pixax/main/urls.py
+++ pixax/main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import AlbumView, RootRedirectView, MyAlbumsView, UploadPicturesView, UnsortedPicturesView, AlbumDeleteView, AlbumEditNameView, AlbumRateSortView, AboutView, UnsortedRateSortView
+from .views import AlbumView, RootRedirectView, MyAlbumsView, UploadPicturesView, UnsortedPicturesView, AlbumDeleteView, AlbumEditNameView, AlbumRateSortIntroView, AboutView, UnsortedRateSortIntroView, UnsortedRateSortActiveView, AlbumRateSortActiveView
 
 app_name = "main"
 
@@ -9,9 +9,11 @@ urlpatterns = [
     path('albums/<int:pk>/', AlbumView.as_view(), name='album'),
     path('albums/<int:pk>/delete/', AlbumDeleteView.as_view(), name='album_delete'),
     path('albums/<int:pk>/edit/', AlbumEditNameView.as_view(), name="album_edit_name"),
-    path('albums/<int:pk>/organise/setup/', AlbumRateSortView.as_view(), name="album_organise"),
+    path('albums/<int:pk>/organise/setup/', AlbumRateSortIntroView.as_view(), name="album_organise"),
+    path('albums/<int:pk>/organise/', AlbumRateSortActiveView.as_view(), name="album_organise_active"),
     path('upload/', UploadPicturesView.as_view(), name="upload"),
     path('albums/unsorted/', UnsortedPicturesView.as_view(), name="unsorted"),
-    path('albums/unsorted/organise/setup/', UnsortedRateSortView.as_view(), name="unsorted_organise"),
+    path('albums/unsorted/organise/setup/', UnsortedRateSortIntroView.as_view(), name="unsorted_organise"),
+    path('albums/unsorted/organise/', UnsortedRateSortActiveView.as_view(), name="unsorted_organise_active"),
     path('about/', AboutView.as_view(), name="about"),
 ]
