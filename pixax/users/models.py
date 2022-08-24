77b9d31b4a140ec8da8cdc5f0a5d7654fdbcdc9a
Olive McCullough <olive.m.mccullough@gmail.com>
2022-08-24 12:14:20 +0100

    Removed application_accepted, replaced with is_active Added post_save to make user inactive by default

--- pixax/users/models.py
+++ pixax/users/models.py
@@ -1,10 +1,18 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 
 class User(AbstractUser):
     email = models.EmailField('email address', unique=True, blank = False, null=False)
-    application_accepted = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.username
+
+@receiver(post_save, sender=User)
+def user_to_inactive(sender, instance, created, update_fields, **kwargs):
+    if created:
+        instance.is_active = False
+        instance.save()
