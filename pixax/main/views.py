9365c8ad8badafffec54ec07dbf077fd54c1be93
Olive McCullough <olive.m.mccullough@gmail.com>
2022-08-24 03:14:07 +0100

    Views for intro sorting reworked Views for active sorting implemented Refactored views to be share base

--- pixax/main/views.py
+++ pixax/main/views.py
@@ -2,12 +2,12 @@ from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.db.models.functions import Lower
 from django.http import Http404
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.generic import RedirectView, CreateView, FormView, ListView, DeleteView, UpdateView, TemplateView
 
-from .forms import AlbumCreateForm, PictureUploadForm, RateAndSortIntroForm
+from .forms import AlbumCreateForm, PictureUploadForm, RateAndSortIntroForm, RateAndSortActiveForm
 from .models import Album, Picture
 
 
@@ -172,7 +172,6 @@ class UnsortedPicturesView(ListView):
 
 class UploadPicturesView(FormView):
     template_name = "upload.html"
-    model = Picture
     form_class = PictureUploadForm
     success_url=reverse_lazy("main:albums")
 
@@ -204,14 +203,64 @@ class UploadPicturesView(FormView):
         kwargs['user'] = self.request.user
         return kwargs
 
-    
-class AlbumRateSortView(FormView):
-    template_name = "album_organise.html"
+
+class RateSortIntroBaseView(FormView):
     form_class = RateAndSortIntroForm
 
     @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
         return super().dispatch(*args, **kwargs)
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        ordering = self.request.GET.get('order_by', '-rating')
+        if not ordering in ["-rating", "rating", "-date_uploaded", "date_uploaded"]:
+            ordering = "-rating"
+        ordering = [ordering, "-id"]
+        kwargs["default_ordering"] = ordering
+        return kwargs
+
+    def form_valid(self, form):
+        form_valid = super().form_valid(form)
+
+        sort_group_id = self.get_sort_group_name()
+        sort_group_order = form.cleaned_data.get("order_select")
+        sort_group_action = form.cleaned_data.get("rate_sort_select")
+        sort_group_limit_rating_to_unrated = form.cleaned_data.get("only_rate_unrated_pics")
+        sort_group_excluded_ids = []
+
+        try:
+            sorting_sets = self.request.session["sorting_sets"]
+        except:
+            sorting_sets= {}
+
+        sorting_set = {"order":sort_group_order, "action": sort_group_action, "limit_rating_to_unrated":sort_group_limit_rating_to_unrated, "excluded_ids":sort_group_excluded_ids}
+
+        sorting_sets[sort_group_id] = sorting_set
+
+        self.request.session["sorting_sets"] = sorting_sets
+
+        return form_valid
+
+
+class UnsortedRateSortIntroView(RateSortIntroBaseView):
+    template_name = "unsorted_organise.html"
+    success_url=reverse_lazy("main:unsorted_organise_active")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        user = self.request.user
+        unsorted_pictures = Picture.objects.filter(user=user, albums=None)
+        if unsorted_pictures.count() == 0:
+            raise Http404
+        return context
+
+    def get_sort_group_name(self):
+        return "unsorted"
+
+
+class AlbumRateSortIntroView(RateSortIntroBaseView):
+    template_name = "album_organise.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data()
@@ -224,38 +273,148 @@ class AlbumRateSortView(FormView):
         context["album"] = album
         return context
 
-    def get_form(self):
-        ordering = self.request.GET.get('order_by', '-rating')
-        if not ordering in ["-rating", "rating", "-date_uploaded", "date_uploaded"]:
-            ordering = "-rating"
-        ordering = [ordering, "-id"]
-        form = RateAndSortIntroForm(initial={'order_select':ordering})
-        return form
+    def get_success_url(self):
+        album_id=self.kwargs['pk']
+        return reverse_lazy('main:album_organise_active', kwargs={'pk': album_id})
+
+    def get_sort_group_name(self):
+        return self.kwargs['pk']
 
 
-class UnsortedRateSortView(FormView):
-    template_name = "unsorted_organise.html"
-    form_class = RateAndSortIntroForm
+class RateSortActiveViewBase(FormView):
+    form_class = RateAndSortActiveForm
+    template_name = "organise_active.html"
 
-    @method_decorator(login_required)
-    def dispatch(self, *args, **kwargs):
-        return super().dispatch(*args, **kwargs)
+    def get(self, request, *args, **kwargs):
+        if self.get_object() == None:
+            return redirect(reverse('main:albums'))
+        get = super().get(request, *args, **kwargs)
+        return get
 
     def get_context_data(self, **kwargs):
-        context = super().get_context_data()
-        user = self.request.user
-        unsorted_pictures = Picture.objects.filter(user=user, albums=None)
-        if unsorted_pictures.count() == 0:
+        sort_group_name = self.get_sort_group_name()
+        context = super().get_context_data(**kwargs)
+        try:
+            ruleset = self.request.session["sorting_sets"][str(sort_group_name)]
+        except:
             raise Http404
+        picture = self.get_object()
+        
+        action = ruleset["action"]
+        limit_rating_to_unrated = ruleset["limit_rating_to_unrated"]
+
+        if action == "rate_only" or action == "both":
+            show_rating = True
+            if limit_rating_to_unrated and picture.rating != None:
+                show_rating = False
+        else:
+            show_rating = False
+
+        if action == "sort_only" or action == "both":
+            show_sort = True
+        else:
+            show_sort = False
+        
+        suggested_albums = picture.suggested_albums.all() | picture.albums.all()
+        other_albums = Album.objects.all().exclude(id__in=suggested_albums)
+
+        suggested_albums=suggested_albums.order_by(Lower("name"))
+        other_albums=other_albums.order_by(Lower("name"))
+
+        context["picture"] = picture
+        context["show_rating"] = show_rating
+        context["show_sort"] = show_sort
+        context["albums_main"] = suggested_albums
+        context["albums_other"] = other_albums
         return context
 
-    def get_form(self):
-        ordering = self.request.GET.get('order_by', '-rating')
-        if not ordering in ["-rating", "rating", "-date_uploaded", "date_uploaded"]:
-            ordering = "-rating"
-        ordering = [ordering, "-id"]
-        form = RateAndSortIntroForm(initial={'order_select':ordering})
-        return form
+    def get_object(self):
+        sort_group_name = self.get_sort_group_name()
+        try:
+            ruleset = self.request.session["sorting_sets"][str(sort_group_name)]
+        except:
+            raise Http404
+        
+        order = ruleset["order"]
+        action = ruleset["action"]
+        limit_rating_to_unrated = ruleset["limit_rating_to_unrated"]
+        excluded_ids = ruleset["excluded_ids"]
+        if action == "rate_only" and limit_rating_to_unrated == True:
+            exclude_rated = True
+        else:
+            exclude_rated = False
+        
+        picture_set = self.base_picture_set()
+        
+        picture_set = picture_set.exclude(id__in=excluded_ids)
+        if exclude_rated:
+            picture_set = picture_set.filter(rating=None)
+        picture_set = picture_set.order_by(order, "-id")
+        if picture_set.count() == 0:
+            return None
+        picture=picture_set.first()
+        return picture
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['user'] = self.request.user
+        kwargs['instance'] = self.get_object()
+        return kwargs
+
+    def form_valid(self, form):
+        sort_group_name = self.get_sort_group_name()
+        form_valid = super().form_valid(form)
+        picture = Picture.objects.filter(id=form.cleaned_data.get("id")).first()
+        user = self.request.user
+        if picture.user_id != user.id:
+            form.add_error("id", "This picture belongs to someone else, you cannot edit it.")
+            return super().form_invalid(form)
+        rating = form.cleaned_data.get("rating")
+        albums = form.cleaned_data.get("albums")
+        picture.rating = rating
+        picture.albums.set(albums)
+        if len(albums)!=0:
+            picture.suggested_albums.clear()
+        picture.save()
+        sorting_sets = self.request.session["sorting_sets"]
+        sorting_set = sorting_sets[str(sort_group_name)]
+
+        excluded_ids = sorting_set["excluded_ids"] 
+        excluded_ids.append(form.cleaned_data.get("id"))
+        
+        sorting_set["excluded_ids"] = excluded_ids
+        
+        sorting_sets[str(sort_group_name)] = sorting_set
+
+        self.request.session["sorting_sets"] = sorting_sets
+
+        return form_valid
+
+
+class AlbumRateSortActiveView(RateSortActiveViewBase):
+    def base_picture_set(self):
+        album_id=self.kwargs['pk']
+        album = Album.objects.filter(id=album_id).first()
+        return album.pictures
+
+
+    def get_success_url(self):
+        album_id = self.kwargs['pk']
+        return reverse_lazy("main:album_organise_active", kwargs={'pk': album_id})
+        
+
+    def get_sort_group_name(self):
+        return self.kwargs['pk']
+
+
+class UnsortedRateSortActiveView(RateSortActiveViewBase):
+    success_url = reverse_lazy("main:unsorted_organise_active")
+
+    def base_picture_set(self):
+        return Picture.objects.filter(albums=None)
+
+    def get_sort_group_name(self):
+        return "unsorted"
 
 
 class AboutView(TemplateView):
