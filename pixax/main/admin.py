5118079c7659d63b3972f2664c3e4c98a165acfe
Olive McCullough <olive.m.mccullough@gmail.com>
2022-08-30 23:07:57 +0100

    Made picture admin clearer and more usable

--- pixax/main/admin.py
+++ pixax/main/admin.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.forms import inlineformset_factory
 from django.contrib import admin
+from django.contrib.admin.decorators import display
 import uuid
 
 from .models import Slideshow, Slide, Picture
@@ -37,6 +38,12 @@ class SlideshowAdmin(admin.ModelAdmin):
 class PictureAdmin(admin.ModelAdmin):
     model = Picture
     readonly_fields = ('image','user', 'date_uploaded', 'albums', 'suggested_albums', 'rating')
+    list_display = ('user', 'get_albums', 'image', 'date_uploaded')
+    search_fields = ['user__username', 'albums__name', 'image']
+
+    @display(description='Albums')
+    def get_albums(self, obj):
+        return "\n".join([album.name for album in obj.albums.all()])
 
 
 admin.site.register(Slideshow, SlideshowAdmin)
